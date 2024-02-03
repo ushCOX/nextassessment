1a81bc566ce85a3a16dab9f46ba6e59c74bd97f9
Ritik <shrestharitik@gmail.com>
2024-02-03 12:19:04 +0545

    implemented login

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -6,10 +8,54 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
+interface LoginForm {
+  username: string;
+  password: string;
+}
 export default function LoginForm() {
+  const [formData, setFormData] = useState<LoginForm>({
+    username: '',
+    password: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const fetchData = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: formData.username,
+          password: formData.password,
+          // expiresInMins: 60, // optional
+        }),
+      });
+      setLoading(false);
+
+      const response = await fetchData.json();
+      if (response.message) {
+        alert(response.message);
+      } else {
+        setEmail(response?.email);
+        alert('Welcome ' + response?.email);
+        router.push('/');
+      }
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
-    <form className="space-y-3">
+    <form className="space-y-3" onSubmit={handleSubmit}>
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -18,17 +64,18 @@ export default function LoginForm() {
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
+              htmlFor="username"
             >
-              Email
+              Username
             </label>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
+                id="username"
+                type="username"
+                name="username"
+                onChange={handleChange}
+                placeholder="Enter your username"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -46,6 +93,7 @@ export default function LoginForm() {
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
+                onChange={handleChange}
                 name="password"
                 placeholder="Enter password"
                 required
@@ -55,7 +103,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <LoginButton />
+        <LoginButton loading={loading} />
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
         </div>
@@ -64,10 +112,14 @@ export default function LoginForm() {
   );
 }
 
-function LoginButton() {
+function LoginButton({ loading }: { loading: boolean | undefined }) {
   return (
-    <Button className="mt-4 w-full">
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    <Button
+      disabled={loading}
+      className={`${loading ? 'opacity-10' : 'opacity-100'} mt-4 w-full`}
+    >
+      {loading ? 'Loading...' : 'Log in'}{' '}
+      <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
