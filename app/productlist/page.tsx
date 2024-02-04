0d1f526316c4d94b0661dc48656886441ce8f6ee
ushCOX <158541586+ushCOX@users.noreply.github.com>
2024-02-04 14:18:40 +0545

    Update page.tsx

--- app/productlist/page.tsx
+++ app/productlist/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
+import Link from 'next/link';
+import NavLinks from '@/app/ui/dashboard/nav-links';
+import AcmeLogo from '@/app/ui/acme-logo';
+import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function Page() {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
+  const [perPage, setPerPage] = useState(50);
   const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
@@ -38,15 +42,28 @@ export default function Page() {
 
   return (
     <main className="text-center">
-{/*       <p className="text-xl">
-        <u className="pb-7  text-blue-900">Product List</u>
-      </p> */}
-      <nav className="px-4 py-4 bg-blue-800 text-white text-center">
-        <ul className="flex">
-            <li className="mx-2 cursor-pointer hover:text-blue-200">Home</li>
-            <li className="mx-2 cursor-pointer hover:text-blue-200">Category</li>
-        </ul>
-    </nav>
+
+
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-700 p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+          <AcmeLogo />
+        </div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((item: { id: string; title: string; imageUrl: string }) => (
           <div key={item.id} className="bg-white p-4 shadow">
